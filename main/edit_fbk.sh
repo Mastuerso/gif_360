15173c06d955ae46a9421ebad1653b3a27b8c946
Antonio Credoz <antonio.credoz@gmail.com>
2017-11-09 12:03:07 -0600

    fbk upload

---
+++ main/edit_fbk.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#modify fbk_upload, upload to facebook
+gif_name=$1
+
+link_arg="7s/.*/\$link_gif=\"http:\/\/www.onikom.com.mx\/gif360\/${gif_name}.gif\";/"
+message_arg="8s/.*/\$message_gif=\"${gif_name}\";/"
+
+#echo $message_arg
+#echo $link_arg
+
+sed -i $link_arg /var/www/html/gif_360_web/server/fbk_upld.php
+sed -i $message_arg /var/www/html/gif_360_web/server/fbk_upld.php
