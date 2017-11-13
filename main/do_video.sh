c54f375381a2247790ea0866d25b2a37c9b91ed1
Antonio.Credoz <antonio.credoz@gmail.com>
2017-11-13 14:18:45 -0600

    do_vid version

--- main/do_video.sh
+++ main/do_video.sh
@@ -3,10 +3,20 @@ gifname=$(date +"%b%d-%H%M")
 w_dir=$(pwd)
 FPS=$(sed '1q;d' "$w_dir/gif_settings.txt")
 FPS=${FPS:6}
+put_MARK=$(sed '10q;d' "$w_dir/gif_settings.txt")
+put_MARK=${put_MARK:10}
 
 ffmpeg -framerate $FPS -pattern_type glob -i $w_dir'/images/*.JPG' $w_dir/$gifname.mp4
 echo $gifname.mp4 >gif_name.txt
 
+if [ $put_MARK -eq $((1)) ]; then
+    MARK=$(sed '11q;d' "$w_dir/gif_settings.txt")
+    MARK=${put_MARK:6}
+    #ffmpeg -i test.mp4 -i watermark.png -filter_complex "overlay=10:10" test1.mp4
+    ffmpeg -i $gifname.mp4 -i $MARK -filter_complex "overlay=x=(main_w-overlay_w)/2:y=(main_h-overlay_h)/2" b$gifname.mp4    
+    rm $wdir/$gifname
+    mv $wdir/b$gifname $wdir/$gifname
+fi
 
 file="$w_dir/results.txt"
 box_ready=false
