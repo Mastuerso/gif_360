e1fdea2a1b87b8ed7c15ac6331ad74e3d5c04056
Smushi <smushi@localhost.localdomain>
2017-11-27 21:33:22 -0600

    more instructions

--- main/file_watchdog.sh
+++ main/file_watchdog.sh
@@ -6,6 +6,9 @@ TASKS=$((0))
 while inotifywait -q -e modify $dir/dummy.sh >/dev/null; do
     echo "filename is changed"
     # do whatever else you need to do
+    # do_video
+    # send_mail
+    # fb_post
     TASKS=$((TASKS + 1))
     echo $TASKS
 done
