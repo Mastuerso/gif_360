b17bb9c2ee71033ded99b44ffa66242147c94b0d
Antonio Credoz <antonio.credoz@gmail.com>
2017-12-05 12:24:43 -0600

    cleanup and improved create and share script

--- main/shell_lab.sh
+++ main/shell_lab.sh
@@ -6,9 +6,15 @@ while [ $DO -eq $((1)) ]; do
     line=$(sed '1q;d' "$dir/chore.list")
     if [ "$line" == "" ]; then
         echo "Nothing to be done" 1>&2
-        remain=$(awk 'NR>1' $dir/chore.list)
-        echo "$remain" > $dir/chore.list
-        exit
+        gif_dir=$(cat "$dir/chore.list")
+        LineCount=$(echo "${gif_dir}" | wc -l)
+        #echo "$LineCount" 1>&2
+        if [ $LineCount -gt $((1)) ]; then
+            remain=$(awk 'NR>1' $dir/chore.list)
+            echo "$remain" > $dir/chore.list
+        else
+            exit
+        fi
     else
         echo "Working on ... $line" 1>&2
         bash "$dir/do_video.sh" "$line"
@@ -20,6 +26,7 @@ while [ $DO -eq $((1)) ]; do
           bash "$dir/fbk_mnger.sh" "$line"
           remain=$(awk 'NR>1' $dir/chore.list)
           echo "$remain" > $dir/chore.list
+          sleep 1s
         fi
     fi    
 done
